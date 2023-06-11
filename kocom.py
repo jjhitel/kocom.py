4e3eee497d21e9e211c399f8e7ec98263ab28bf5
Jeong Seongpil <jjjhitel@gmail.com>
2023-06-11 23:03:24 +0900

    Update kocom.py

--- kocom.py
+++ kocom.py
@@ -301,7 +301,7 @@ def light_parse(value):
 def plug_parse(value):
     ret = {}
     for i in range(1, int(config.get('User', 'plug_count'))+1):
-        ret['plug_'+str(i)] = 'off' if value[i*2-2:i*2] == '00' else 'on'
+        ret['plug_'+str('0')] = 'on' if value[i*2-2:i*2] > 0 else 'off'
     return ret
 
 
@@ -693,7 +693,7 @@ def publish_discovery(dev, sub=''):
                 logging.info(logtxt)
     elif dev == 'plug':
         for num in range(1, int(config.get('User', 'plug_count'))+1):
-            #ha_topic = 'homeassistant/plug/kocom_livingroom_plug1/config'
+            #ha_topic = 'homeassistant/switch/kocom_livingroom_plug1/config'
             topic = 'homeassistant/switch/kocom_{}_plug{}/config'.format(sub, num)
             payload = {
                 'name': 'Kocom {} Plug{}'.format(sub, num),
